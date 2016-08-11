0537b566f957365e86d31475a0a5a233bbca2917
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2016-08-11 20:24:23 +0200

    init

---
+++ src/Syscover/Mirai/MiraiServiceProvider.php
@@ -0,0 +1,26 @@
+<?php namespace Syscover\Comunik;
+
+use Illuminate\Support\ServiceProvider;
+
+class MiraiServiceProvider extends ServiceProvider
+{
+	/**
+	 * Bootstrap the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+
+	}
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+        //
+	}
+}
